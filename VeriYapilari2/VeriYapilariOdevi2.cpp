1530b3923836d35d06ebc62389e0dca307cb814d
MuhammedAliAgdas <132196769+MuhammedAliAgdas@users.noreply.github.com>
2024-01-24 15:55:34 +0300

    end project

---
+++ VeriYapilari2/VeriYapilariOdevi2.cpp
@@ -0,0 +1,193 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+struct dugum
+{
+	int plakaNo;
+	string sehir;
+	struct dugum* solDügüm;
+	struct dugum* sagDügüm;
+	dugum(int no,string isim) :plakaNo(no),sehir(isim), solDügüm(nullptr), sagDügüm(nullptr) {}
+};
+
+dugum* kokArama(dugum* agacKok, int no)
+{
+	if (agacKok == nullptr || agacKok->plakaNo == no) { return agacKok; }
+	if (no < agacKok->plakaNo) { return kokArama(agacKok->solDügüm, no); }
+	else { return kokArama(agacKok->sagDügüm, no); }	
+}
+
+void agacaEkle(dugum*& agacKok, int no , string isim)
+{
+	if (agacKok == nullptr) { agacKok = new dugum(no, isim);}
+	if (no < agacKok->plakaNo) { agacaEkle(agacKok->solDügüm, no, isim);}
+	else if (no > agacKok->plakaNo) { agacaEkle(agacKok->sagDügüm, no, isim);}
+}
+
+int derinlikHesap(dugum* agacKok)
+{
+	if (agacKok == nullptr) { return 0; }
+	int solDerinlik = derinlikHesap(agacKok->solDügüm);
+	int sagDerinlik = derinlikHesap(agacKok->sagDügüm);
+	return 1 + max(solDerinlik, sagDerinlik);
+}
+
+bool dengeliMi(dugum* agacKok)
+{
+	int solDerinlik = derinlikHesap(agacKok->solDügüm);
+	int sagDerinlik = derinlikHesap(agacKok->sagDügüm);
+	if (abs(solDerinlik - sagDerinlik) > 1) { return false; }
+	else { return true; }
+	return dengeliMi(agacKok->solDügüm) && dengeliMi(agacKok->sagDügüm);
+}
+
+void sehirIsmiYazma(dugum* agacKok, int no)
+{
+	dugum* dugum = kokArama(agacKok, no);
+	if (dugum == nullptr) { cout << "Plaka kodu bulunamadi." << endl; }
+	else { cout << "Sehir: " << dugum->sehir << endl; }
+}
+
+int agacDuzeyFarki(dugum* agacKok, int plaka1, int plaka2)
+{
+	dugum* dugum1 = kokArama(agacKok, plaka1);
+	dugum* dugum2 = kokArama(agacKok, plaka2);
+	if (dugum1 == nullptr || dugum2 == nullptr) { return -1; }
+	int duzey1 = 0, duzey2 = 0;
+	dugum* sonDugum = agacKok;
+	while (sonDugum != nullptr && sonDugum->plakaNo != plaka1)
+	{
+		if (plaka1 < sonDugum->plakaNo)
+		{
+			sonDugum = sonDugum->solDügüm;
+			duzey1++;
+		}
+		else
+		{
+			sonDugum = sonDugum->sagDügüm;
+			duzey1++;
+		}
+	}
+	sonDugum = agacKok;
+	while (sonDugum != nullptr && sonDugum->plakaNo != plaka2)
+	{
+		if (plaka2 < sonDugum->plakaNo)
+		{
+			sonDugum = sonDugum->solDügüm;
+			duzey2++;
+		}
+		else 
+		{
+			sonDugum = sonDugum->sagDügüm;
+			duzey2++;
+		}
+	}
+	return abs(duzey1 - duzey2);
+}
+
+void agacıYaz()
+{
+	int bosluk = 28;
+	int index = 0;
+	int okunaneleman = 1;
+	int plakalar[] = { 55,29,69,19,52,60,78,8,28,37,53,57,67,74,81,5,14,61 };
+	for (int i = 0; i < 18; i++)
+	{
+		cout << setw(bosluk) << plakalar[i];
+		index++;
+		if (index == okunaneleman)
+		{
+			cout << endl ;
+			okunaneleman *= 2;
+			index = 0;
+			bosluk -= 8;
+		}
+		if (i == 6) { bosluk += 3; }
+		if (i == 10) { bosluk -= 1; }
+		if (i == 14) { bosluk += 7; }
+		if (i == 16) { bosluk += 23; }
+	}
+}
+
+void preOrder(struct dugum* agacKok)
+{
+	if (agacKok == nullptr) return;
+	cout << agacKok->plakaNo << "-";
+	preOrder(agacKok->solDügüm);
+	preOrder(agacKok->sagDügüm);
+}
+void inOrder(dugum* agacKok)
+{
+	if (agacKok == nullptr) return;	
+	inOrder(agacKok->solDügüm);
+	cout << agacKok->plakaNo << "-";
+	inOrder(agacKok->sagDügüm);
+}
+void postOrder(struct dugum* agacKok)
+{
+	if (agacKok == nullptr) return;
+	postOrder(agacKok->solDügüm);
+	postOrder(agacKok->sagDügüm);
+	cout << agacKok->plakaNo << "-";
+}
+
+int main()
+{
+	ifstream dosyaOku("sehir_plaka_kodlari.txt");
+	string harf = "";
+	string plakaVeSehir[18][18];
+	int harfSayaci = 0;
+	dugum* agacKok = nullptr;
+	if (dosyaOku.is_open())
+	{
+		while (getline(dosyaOku, harf) && harfSayaci < 18)
+		{
+			if (harf.length() >= 2)
+			{
+				plakaVeSehir[harfSayaci][0] = harf.substr(0, 2);
+				plakaVeSehir[harfSayaci][1] = harf.substr(3, 9);
+				harfSayaci++;
+			}
+		}
+		dosyaOku.close();
+	}
+	for (int i = 0; i < 18; i++)
+	{
+		agacaEkle(agacKok, stoi(plakaVeSehir[i][0]), plakaVeSehir[i][1]);
+	}
+
+	int derinlik = derinlikHesap(agacKok);
+	cout << "Agacin derinligi: " << derinlik << endl;
+	bool denge = dengeliMi(agacKok);
+	cout << "Agac dengeli mi?" << (denge ? " Dengeli" : " Dengesiz") << endl<<endl;
+	agacıYaz();
+	cout << endl<<"---------------------------------------------------------"<<endl<<endl;
+	
+	int plakaNo1, plakaNo2;
+	cout << "Birinci plaka kodu girin: ";
+	cin >> plakaNo1;
+	cout << "Ikinci plaka kodu girin: ";
+	cin >> plakaNo2;
+
+	sehirIsmiYazma(agacKok, plakaNo1);
+	sehirIsmiYazma(agacKok, plakaNo2);
+	int seviyeFarki = agacDuzeyFarki(agacKok, plakaNo1, plakaNo2);
+	if (seviyeFarki != -1) { cout << "Duzey farki: " << seviyeFarki << endl << endl; }
+	else { cout << "2 plaka kodundan en az biri agacta yok ise duzey farklarni gösteremiyoruz." << endl<< endl; }
+
+	cout << "inorder: ";
+	inOrder(agacKok);
+	cout << endl;
+	cout << "preorder: ";
+	preOrder(agacKok);
+	cout << endl;
+	cout << "postorder: ";
+	postOrder(agacKok);
+	cout << endl;
+
+	return 0;
+
+}
---
+++ VeriYapılari1/VeriYapılariOdev.cpp
@@ -0,0 +1,247 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+//normal kuyruk islemleri...
+struct normalKuyruk
+{
+    int musteriNo;
+    int islemSuresi;
+    normalKuyruk* sonraki;
+    normalKuyruk* sonrakiSure;
+};
+normalKuyruk* SureDugum = new normalKuyruk;
+normalKuyruk* son = new normalKuyruk;
+normalKuyruk* oncekiBeklemeSuresi = new normalKuyruk;
+int kuyrukBoyutu = 0;
+int kuyrukSirasi = 0;
+
+void normalKuyrugaEkle(int no,int sure)
+{
+    normalKuyruk* yeniNo = new normalKuyruk;
+    yeniNo->musteriNo = no;
+    normalKuyruk* yeniSure = new normalKuyruk;
+    yeniSure->islemSuresi = sure;
+    if (kuyrukBoyutu == 0)
+    {
+        yeniNo->sonraki = NULL;
+        yeniSure->sonrakiSure = NULL;
+    }
+    else
+    {
+        yeniNo->sonraki = son;
+        yeniSure->sonrakiSure = SureDugum;
+    }
+    oncekiBeklemeSuresi->islemSuresi += SureDugum->islemSuresi;
+    son = yeniNo;
+    SureDugum = yeniSure;
+    kuyrukBoyutu++;
+}
+int toplamSure=0;
+void normalKuyruktanSil()
+{
+    kuyrukSirasi++;
+    if (kuyrukBoyutu > 0)
+    {
+        normalKuyruk* yeniSon = son;
+        normalKuyruk* yeniSure = SureDugum;
+        toplamSure = toplamSure + yeniSure->islemSuresi;
+        for (int i = 1; i < kuyrukBoyutu; i++)
+        {
+            yeniSon = yeniSon->sonraki;
+            yeniSure = yeniSure->sonrakiSure;
+        }
+        if (kuyrukSirasi == 1) { oncekiBeklemeSuresi->islemSuresi = 0; }
+        cout <<kuyrukSirasi<<". sirada kuyruktan cikan " << yeniSon->musteriNo << "'no lu musterinin islemi ";
+        cout << yeniSure->islemSuresi << "sn. surdu ve islem suresi ile birlikte " << (yeniSure->islemSuresi + oncekiBeklemeSuresi->islemSuresi) << "sn. kuyrukta bekledi. " << endl << endl;
+        yeniSon->sonraki = NULL;
+        yeniSure->sonrakiSure = NULL;
+        kuyrukBoyutu--;
+    }
+    else
+    {
+        cout <<"Musterilerin ortalama islem suresi: " << toplamSure/(kuyrukSirasi-1) <<"sn. dir." << endl<<endl;
+    }
+}
+
+struct listeYigin
+{
+    int musteriNo;
+    int musteriSure;
+    listeYigin* sonraki;
+    listeYigin* sonrakiSure;
+};
+
+listeYigin* yiginTepesi = new listeYigin;
+listeYigin* yiginSureTepesi = new listeYigin;
+int yiginUzunlugu = 0;
+
+void listeYiginaEkle(int no,int sure)
+{
+    listeYigin* yeniNo = new listeYigin;
+    listeYigin* yeniSure = new listeYigin;
+    yeniNo->musteriNo = no;
+    yeniSure->musteriSure = sure;
+    yeniNo->sonraki = yiginTepesi;
+    yeniSure->sonrakiSure = yiginSureTepesi;
+    yiginTepesi = yeniNo;
+    yiginSureTepesi = yeniSure;
+    yiginUzunlugu++;
+}
+int listeYigindanCikar()
+{
+    if (yiginUzunlugu != 0)
+    {
+        normalKuyrugaEkle(yiginTepesi->musteriNo, yiginSureTepesi->musteriSure);
+        yiginTepesi = yiginTepesi->sonraki;
+        yiginSureTepesi = yiginSureTepesi->sonrakiSure;
+        yiginUzunlugu--;
+        return 0;
+    }
+}
+
+//Onlelikli kuyruk islemleri...
+
+#define oncelikliKuyrukUzunlugu 10
+struct oncelikliKuyruk
+{
+    int musteriNumaralari[oncelikliKuyrukUzunlugu];
+    int musteriSayisi = 0;
+    int kuyrukBasi = 0;
+    int kuyrukSonu = -1;
+    int islemSureleri[oncelikliKuyrukUzunlugu];
+    int toplamSure = 0, ortalamaSure = 0;
+};
+
+void oncelikliKuyrugaEkle(oncelikliKuyruk* k, int musteriNo, int musteriSuresi)
+{
+    k->kuyrukSonu++;
+    if (k->kuyrukSonu == oncelikliKuyrukUzunlugu)
+    {
+        k->kuyrukSonu = 0;
+    }
+    k->musteriNumaralari[k->kuyrukSonu] = musteriNo;
+    k->islemSureleri[k->kuyrukSonu] = musteriSuresi;
+    k->musteriSayisi++;
+}
+int beklenensureler = 0;
+int sayac = 0;
+void oncelikliKuyruktanCikar(oncelikliKuyruk* k)
+{
+    int sureDegisimi;
+    int musteriNoDegisimi;
+    for (int i = 1; i < k->musteriSayisi; i++)
+    {
+        for (int j = 0; j < (k->musteriSayisi - i); j++)
+        {
+            if (k->islemSureleri[j] > k->islemSureleri[j + 1])
+            {
+                sureDegisimi = k->islemSureleri[j];
+                k->islemSureleri[j] = k->islemSureleri[j + 1];
+                k->islemSureleri[j + 1] = sureDegisimi;
+
+                musteriNoDegisimi = k->musteriNumaralari[j];
+                k->musteriNumaralari[j] = k->musteriNumaralari[j + 1];
+                k->musteriNumaralari[j + 1] = musteriNoDegisimi;
+            }
+        }
+    }
+    beklenensureler += k->islemSureleri[sayac];
+    sayac++;
+    cout << k->musteriNumaralari[k->kuyrukBasi] << "no'lu musterinin islemi ";
+    cout << k->islemSureleri[k->kuyrukBasi] << "sn. surdu ve islem suresi ile birlikte " << beklenensureler << "sn. kuyrukta bekledi." << endl;
+    k->kuyrukBasi++;
+    if (k->kuyrukBasi == oncelikliKuyrukUzunlugu)
+    {
+        k->kuyrukBasi = 0;
+    }
+    k->musteriSayisi--;
+}
+
+void ortalamaSureHesap(oncelikliKuyruk* k, int diziBuyuklugu)
+{
+    for (int i = 0; i < diziBuyuklugu; i++)
+    {
+        k->toplamSure += k->islemSureleri[i];
+    }
+    k->ortalamaSure = k->toplamSure / diziBuyuklugu;
+    cout << endl << "Musterilerin bekleme sure ortalamasi:  " << k->ortalamaSure <<"sn. dir." << endl;
+}
+
+#define ikinciYiginBuyuklugu 100
+struct diziYigin
+{
+    int musteriNumaralari[ikinciYiginBuyuklugu];
+    int islemSureleri[ikinciYiginBuyuklugu];
+    int yiginTepesi = -1;
+    oncelikliKuyruk kuyrukIsareti;
+};
+void diziYiginaEkle(diziYigin* y, int no,int sure)
+{
+    y->yiginTepesi++;
+    y->musteriNumaralari[y->yiginTepesi] = no;
+    y->islemSureleri[y->yiginTepesi] = sure;
+}
+void diziYigindanCikar(diziYigin* y)
+{
+    oncelikliKuyrugaEkle(&y->kuyrukIsareti, y->musteriNumaralari[y->yiginTepesi], y->islemSureleri[y->yiginTepesi]);
+    y->yiginTepesi--;
+}
+
+
+
+int main()
+{
+    diziYigin* yiginIsareti = new diziYigin;
+    int secim, musteriNo, normalKuyrukMusteriSayisi = 0, oncelikliKuyrukMusteriSayisi = 0;
+    int  randomSayi=0, maxSure = 300, minSure = 30;
+    bool ilkMi = true;
+    do
+    {
+        cout << "0-Eklenecek musteri kalmadi. " << " 1-Musteri Ekle." << endl;
+        cin >> secim;
+        if (secim == 1)
+        {
+            srand(time(0));
+            cout << "Musteri no'su gir: " << endl;
+            cin >> musteriNo;
+            cout << musteriNo << " no'lu musteri siraya girdi." << endl;
+            for (int i = 0; i < (oncelikliKuyrukMusteriSayisi+1); i++)
+            {
+                randomSayi = rand() % (maxSure - minSure + 1) + minSure;
+            }
+            listeYiginaEkle(musteriNo, randomSayi);
+            diziYiginaEkle(yiginIsareti, musteriNo, randomSayi);
+            ilkMi = false;
+            normalKuyrukMusteriSayisi++;
+            oncelikliKuyrukMusteriSayisi++;
+        }
+        if (secim != 0 && secim != 1) { cout << "Yanlis deger girdiniz..." << endl; secim = 1; }
+        if (ilkMi) { cout << "En az 1 secim yapilmali." << endl; secim = 1; }
+    } while (secim == 1);
+    for (int i = 0; i < oncelikliKuyrukMusteriSayisi; i++)
+    {
+        diziYigindanCikar(yiginIsareti);
+    }
+    cout << "-------------" << endl << endl;
+    cout << "Normal kuyruk ile Musterilerin islemleri yapiliyor..." << endl << endl;
+    cout << "-------------" << endl << endl;
+    do
+    {
+        listeYigindanCikar();
+        normalKuyruktanSil();
+        normalKuyrukMusteriSayisi--;
+
+    } while (normalKuyrukMusteriSayisi !=-1);
+    cout << "-------------" << endl << endl;
+    cout << "Oncelikli kuyruk ile Musterilerin islemleri yapiliyor..." << endl << endl;
+    cout << "-------------" << endl;
+
+    for (int i = 0; i < oncelikliKuyrukMusteriSayisi; i++)
+    {
+        cout << endl << (i + 1) << ". sirada kuyruktan cikan ";
+        oncelikliKuyruktanCikar(&yiginIsareti->kuyrukIsareti);
+    }
+
+    ortalamaSureHesap(&yiginIsareti->kuyrukIsareti, oncelikliKuyrukMusteriSayisi);
+}
